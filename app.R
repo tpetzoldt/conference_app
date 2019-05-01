81cd938975af865be9799f81ae88cd0c03dabd95
Harvey Lieberman <harvey.lieberman@gmail.com>
2019-04-30 23:33:25 -0400

    add agenda

--- app.R
+++ app.R
@@ -82,8 +82,41 @@ f7Tabs2 <- function(..., swipeable = FALSE, animated = TRUE, scrollable = FALSE)
   
 }
 
+## create a dummy agenda
+df.agenda <- data.frame(
+  day = rep(paste0('2019-08-', 21:23), each=5),
+  time = rep(paste0(10:14, ':00-', 11:15, ':00'), 3),
+  agendaitem = rep(paste0('Agenda item ', seq(5)), 3),
+  stringsAsFactors = FALSE
+)
+
 
 server <- function(input, output, session) {
+  
+  ## render convference agenda
+  output$tab_agenda <- renderUI({
+    l.agenda <- split.data.frame(df.agenda, df.agenda$day)
+    l.agenda_day <- lapply(seq_along(l.agenda), function(i) {
+      f7AccordionItem(
+        title = names(l.agenda)[i],
+        tagList(
+          apply(l.agenda[[i]], 1, function(y) {
+            f7Row(h4(y['day'], style = 'color: #b71c1c'), h4(y['time'], style = 'color: #0D47A1'), h4('agendaitem'), br(), h4('AUTHOR NAME'), hr())
+          })
+        )
+      )
+    })
+    
+    f7Shadow(
+      intensity = 10,
+      hover = TRUE,
+      f7Accordion(
+        mode = 'list',
+        l.agenda_day
+      )
+    )
+  })
+  
   
 }
 
@@ -112,21 +145,9 @@ ui <- f7Page(
                    h4('profile')
                    )),
            f7Tab(tabName = 'Agenda', icon = 'event_available', active = FALSE,
-                 f7Card(
-                   h4('agenda')
+                 f7Card(title = 'Agenda',
+                   uiOutput('tab_agenda')
                    )),
-           f7Tab(tabName = 'Attendees', icon = 'people', active = FALSE,
-                 f7Card(
-                   h4('attendees')
-                   )),
-           f7Tab(tabName = 'Attendees', icon = 'people', active = FALSE,
-                 f7Card(
-                   h4('attendees')
-                 )),
-           f7Tab(tabName = 'Attendees', icon = 'people', active = FALSE,
-                 f7Card(
-                   h4('attendees')
-                 )),
            f7Tab(tabName = 'Attendees', icon = 'people', active = FALSE,
                  f7Card(
                    h4('attendees')
