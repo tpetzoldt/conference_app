e6d0598bbd6fd4e33918470c62edb2f1213a4687
Harvey Lieberman <harvey.lieberman@gmail.com>
2019-05-01 15:58:30 -0400

    user profile

--- modules/module_profile.R
+++ modules/module_profile.R
@@ -1,5 +1,13 @@
 ## Profile Module
 
+
+## demo user data
+user_name <- 'bob'
+user_email <- 'bob@bob.com'
+user_image <- 'https://randomuser.me/api/portraits/men/32.jpg'
+user_bio <- ''
+
+
 ## UI for Profile Module
 module_profileUI <- function(id) {
   ns <- NS(id)
@@ -8,9 +16,20 @@ module_profileUI <- function(id) {
 
 ## server function for Profile Module
 module_profile <- function(input, output, session) {
+  
+  ns <- session$ns
+  
   output$uiProfile <- renderUI({
-    f7Card(title = 'Profile',
-           h4('profile')
-    )
+    f7Card(title = user_name,
+           div(style = 'text-align: center;',
+               tags$div(style = 'height: 80px; width: 80px; display: inline-block;',
+                        tags$img(style='height: 100%; width: 100%; object-fit: contain', src=user_image)
+                        ),
+               br(),
+               h3(user_name),
+               h3(user_email)
+               )
+           )
   })
+
 }
