d407584744e2de6df5f7f7602aca24698ee8ece2
Harvey Lieberman <harvey.lieberman@gmail.com>
2019-05-01 16:28:34 -0400

    f7InputList - create an input list containing text, password, textarea and select

--- shinyF7_extend.R
+++ shinyF7_extend.R
@@ -84,3 +84,69 @@ tab_select <- function(session, tab = NULL) {
 }
 
 
+## input list: a list of inputs
+## can currently render text, password, textarea and select
+f7InputList <- function(inputList = list(), outline = FALSE, labels = 'stacked') {
+  
+  if (labels == 'inline') {
+    label_class = 'item-title inline-label'
+  } else if (labels == 'floating') {
+    label_class = 'item-title item-floating-label'
+  } else {
+    label_class = 'item-title item-label'
+  }
+  
+  li_class <- paste0("item-content item-input", ifelse(outline == TRUE, " item-input-outline", ""))
+  
+  div(class = "list",
+      tags$ul(
+        lapply(inputList, function(x) {
+          
+          if (x$type == "text") {
+            input_details <- tags$input(type = "text", id = x$inputId, x$value)
+          } else if (x$type == "password") {
+            input_details <- tags$input(type = "password", id = x$inputId)
+          } else if (x$type == "textarea") {
+            input_details <- tags$textarea(id = x$inputId, x$value)
+          } else if (x$type == 'select') {
+            input_details <- tagList(
+              tags$select(id = x$inputId,
+                          tagList(lapply(x$choices, function(x) tags$option(value=x, x)))
+              )
+            )
+          }
+          
+          if (.getTrue(x$clearButton, TRUE)) {
+            input_details <- tagList(input_details, tags$span(class = "input-clear-button"))
+          }
+          
+          tags$li(class = li_class,
+                  div(class = "item-inner",
+                      div(class = label_class, x$label),
+                      div(class = "item-input-wrap form-group shiny-input-container",
+                          input_details
+                      )
+                  ))
+        })
+      )
+  )
+}
+
+
+## return true if x == validate and not null
+.getTrue <- function(x, validate) {
+  if (!is.null(x)) {
+    if (x == validate) {
+      return(TRUE)
+    } else {
+      return(FALSE)
+    }
+  } else {
+    return(FALSE)
+  }
+}
+
+
+
+
+
