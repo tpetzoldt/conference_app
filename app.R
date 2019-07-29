155f36579bf8dac3abb46d9a39d229df8fe43889
James Black <james.black.jb2@roche.com>
2019-07-29 11:04:03 +0200

    move to data

--- app.R
+++ app.R
@@ -6,7 +6,7 @@ library(shinyF7)
 library(pool)
 library(dplyr)
 
-print(file.exists('conference_app.sqlite'))
+print(file.exists('data/conference_app.sqlite'))
 
 pool <- dbPool(
   drv = RSQLite::SQLite(),
